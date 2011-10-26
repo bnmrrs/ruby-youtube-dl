32b0db77b7b0131dedef881ab5f4503acaa3746b
Ben Morris <ben@bnmrrs.com>
2011-10-26 16:30:33 -0400

    more altering of command line flags

--- lib/ruby-youtube-dl/downloader.rb
+++ lib/ruby-youtube-dl/downloader.rb
@@ -1,11 +1,11 @@
 module YoutubeDL
   class Downloader
     def self.download_video(url)
-      `ruby-youtube-dl --no-progress --get-filename -q #{url}`
+      `ruby-youtube-dl --no-progress --get-filename -q #{url}`.strip
     end
 
     def self.download_audio(url)
-      `ruby-youtube-dl --no-progress --get-filename --extract-audio --audio-format=mp3 -tAqs #{url}`
+      `ruby-youtube-dl --no-progress --get-filename --extract-audio --audio-format=mp3 -tAqs #{url}`.strip
     end
   end
 end
