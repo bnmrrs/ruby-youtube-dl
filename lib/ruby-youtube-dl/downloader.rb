45c852f304b2fa6d58a20f125d52a860fa667111
Ben Morris <ben@bnmrrs.com>
2011-10-26 16:14:42 -0400

    Corrected name for downloader

--- lib/ruby-youtube-dl/downloader.rb
+++ lib/ruby-youtube-dl/downloader.rb
@@ -1,11 +1,11 @@
 module YoutubeDL
   class Downloader
     def self.download_video(url)
-      `youtube-dl --no-progress #{url}`
+      `ruby-youtube-dl --no-progress #{url}`
     end
 
     def self.download_audio(url)
-      `youtube-dl --no-progress --extract-audio --audio-format=mp3 -tA #{url}`
+      `ruby-youtube-dl --no-progress --extract-audio --audio-format=mp3 -tA #{url}`
     end
   end
 end
