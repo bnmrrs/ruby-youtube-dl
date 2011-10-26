4dd943f84631757dfe5acb7e6669eff8ceb994f1
Ben Morris <ben@bnmrrs.com>
2011-10-26 17:43:48 -0400

    Added a way to change the filepath for audio downloading

--- lib/ruby-youtube-dl/downloader.rb
+++ lib/ruby-youtube-dl/downloader.rb
@@ -4,8 +4,8 @@ module YoutubeDL
       `ruby-youtube-dl --no-progress --get-filename -q #{url}`.strip
     end
 
-    def self.download_audio(url)
-      `ruby-youtube-dl --no-progress --get-filename --extract-audio --audio-format=mp3 -tAq #{url}`.strip
+    def self.download_audio(url, location="/tmp/")
+      `./youtube-dl.py --no-progress --extract-audio --audio-format=mp3  --output="#{location}%(stitle)s-%(uploader)s-%(autonumber)s.mp3" #{url}`.strip
     end
   end
 end
