d714aa4f6f192428c85987c0d57b3415d5886607
Ben Morris <ben@bnmrrs.com>
2011-10-26 16:11:15 -0400

    Added Downloader

--- lib/ruby-youtube-dl/downloader.rb
+++ lib/ruby-youtube-dl/downloader.rb
@@ -1,10 +1,10 @@
 module YoutubeDL
   class Downloader
-    def download_video(url)
+    def self.download_video(url)
       `youtube-dl --no-progress #{url}`
     end
 
-    def download_audio(url)
+    def self.download_audio(url)
       `youtube-dl --no-progress --extract-audio --audio-format=mp3 -tA #{url}`
     end
   end
